637f7c341394090543bf3a671f52fdbe0c064c07
rebahama <wiri@hotmail.se>
2023-11-14 01:02:18 +0000

    create pokemon data in js file to make ready for displaying data in html

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,1 +1,74 @@
-console.log("hello")
+const pokemonData = [
+    {
+      name: "Pikachu",
+      price: 25.99,
+      rating: 4.5,
+      category: "Electric",
+      image: "pikachu.jpg" // Add the image file name or URL
+    },
+    {
+      name: "Charizard",
+      price: 49.99,
+      rating: 4.8,
+      category: "Fire/Flying",
+      image: "charizard.jpg"
+    },
+    {
+      name: "Bulbasaur",
+      price: 19.99,
+      rating: 4.2,
+      category: "Grass/Poison",
+      image: "bulbasaur.jpg"
+    },
+    {
+      name: "Squirtle",
+      price: 22.99,
+      rating: 4.3,
+      category: "Water",
+      image: "squirtle.jpg"
+    },
+    {
+      name: "Jigglypuff",
+      price: 18.99,
+      rating: 4.1,
+      category: "Normal/Fairy",
+      image: "jigglypuff.jpg"
+    },
+    {
+      name: "Mewtwo",
+      price: 59.99,
+      rating: 4.9,
+      category: "Psychic",
+      image: "mewtwo.jpg"
+    },
+    {
+      name: "Eevee",
+      price: 29.99,
+      rating: 4.6,
+      category: "Normal",
+      image: "eevee.jpg"
+    },
+    {
+      name: "Snorlax",
+      price: 39.99,
+      rating: 4.7,
+      category: "Normal",
+      image: "snorlax.jpg"
+    },
+    {
+      name: "Mew",
+      price: 69.99,
+      rating: 5.0,
+      category: "Psychic",
+      image: "mew.jpg"
+    },
+    {
+      name: "Gengar",
+      price: 34.99,
+      rating: 4.4,
+      category: "Ghost/Poison",
+      image: "gengar.jpg"
+    }
+  ];
+
+  console.log(pokemonData)
